a18bc0efcf61b3165d3d07745d63dca3c05ae2ad
Pavel Sychev <pasha.sychev@gmail.com>
2015-09-30 00:51:19 +0300

    Better run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
-./cache 1023
-./cache 1024
-./cache 1025
-./cache 1055
+#!/usr/bin/env bash
+matrixSizes=( 127 128 129 1023 1024 1025 1040 1041 1050 1100 )
+cacheSizes=( 32 256 3072 )
+cacheLineSizes=( 8 8 12 )
+for matrixSize in "${matrixSizes[@]}"
+do
+    for i in `seq 3`
+    do
+        echo '$> ./cache' $matrixSize ${cacheSizes[$i-1]} 64 ${cacheLineSizes[$i-1]}
+        ./cache $matrixSize ${cacheSizes[$i-1]} 64 ${cacheLineSizes[$i-1]}
+    done
+done
